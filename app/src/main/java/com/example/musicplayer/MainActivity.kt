0fd2950408d93493e82931f8b164c71c05350c45
RikitoNoto <rikitonoto@gmail.com>
2020-06-04 20:00:01 +0900

    ストレージから再生する機能を追加

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,25 +1,82 @@
 package com.example.musicplayer
 
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_EXTERNAL_STORAGE: Int = 1
+    val PERMISSIONS_STORAGE: Array<String> = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val permission:Int = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (permission != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE)
+        }
 
-        val intent = Intent(this, PlayScreen::class.java)
-        intent.putExtra("MUSIC", R.raw.pastel_house)
-        startActivity(intent)
+        val (id, title) = searchMusicUrl(contentResolver)
+        if(id != null)
+            {
+                val intent = Intent(this, PlayScreen::class.java)
+                intent.putExtra("TITLE", title)
+                intent.putExtra("id", id)
+                startActivity(intent)
+            }
     }
+
+    fun searchMusicUrl(resolver: ContentResolver): Pair<Long?, String?>
+    {
+        var title: String? = null
+        var id: Long? = null
+        Log.d("debugLog", "called")
+        val cursor:Cursor? = resolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+        when {
+            cursor == null -> {
+                // query failed, handle error.
+            }
+            !cursor.moveToFirst() -> {
+                // no media on the device
+            }
+            else -> {
+                val titleColumn: Int = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE)
+                val idColumn: Int = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID)
+                title = cursor.getString(titleColumn)
+                id = cursor.getLong(idColumn)
+//                do {
+//                    val thisId = cursor.getLong(idColumn)
+//                    val thisTitle = cursor.getString(titleColumn)
+//                    // ...process entry...
+//                } while (cursor.moveToNext())
+            }
+        }
+        cursor?.close()
+        return Pair(id, title)
+    }
+
 }
--- app/src/main/java/com/example/musicplayer/PlayScreen.kt
+++ app/src/main/java/com/example/musicplayer/PlayScreen.kt
@@ -1,31 +1,48 @@
 package com.example.musicplayer
 
+import android.content.ContentUris
+import android.media.AudioManager
 import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import kotlinx.android.synthetic.main.activity_play_screen.*
 
 class PlayScreen : AppCompatActivity() {
-    private lateinit var music : MediaPlayer
+    private lateinit var musicPlayer : MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_screen)
 
-        music = MediaPlayer.create(this, intent.getIntExtra("MUSIC", 0))
-        playButton.setOnClickListener{ PlayMusic() }
+        val uri = ContentUris.withAppendedId(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, intent.getLongExtra("id", 0))
+        musicPlayer = MediaPlayer().apply {
+            setDataSource(applicationContext, uri)
+            prepareAsync()
+            setOnPreparedListener{
+                playButton.setOnClickListener{ PlayMusic() }
+            }
+        }
     }
 
     fun PlayMusic()
     {
-        music.start()
+        musicPlayer.start()
         playButton.setImageResource(R.drawable.pause)
         playButton.setOnClickListener{ PauseMusic()}
     }
 
     fun PauseMusic()
     {
-        music.pause()
+        musicPlayer.pause()
         playButton.setImageResource(R.drawable.play)
         playButton.setOnClickListener{ PlayMusic() }
+    }
+
+    fun test()
+    {
+        val media = MediaStore.Audio.Media()
+//        media.getContentUri()
     }
 }
